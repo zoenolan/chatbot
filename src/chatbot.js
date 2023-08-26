fae0b2fb77e59abbb0c37579693714e3227a91ce
Zoe Nolan <github@zoenolan.org>
2023-08-26 01:33:24 +0100

    Add the chatbot example code

---
+++ src/chatbot.js
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+"use strict";
+
+const rulesBased = require("./rules.js");
+
+class Chatbot {
+    constructor(rulesFile) {
+        this.rulesBased = new rulesBased.RulesBased(rulesFile);
+
+    }
+
+    async process(input) {  
+        const reply = await this.rulesBased.process(input);
+
+        return reply;
+    }  
+};
+
+exports.Chatbot = Chatbot;
